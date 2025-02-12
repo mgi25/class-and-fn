8e17012993f9ae99f3fa626e3f466a26a1b55933
MGI <alen.shibu@bscdsh.christuniversity.in>
2025-02-12 10:46:54 +0530

    update

--- asg.py
+++ asg.py
@@ -12,6 +12,28 @@ import sqlite3
 
 app = Flask(__name__)
 
+DATABASE = 'task'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+with app.app_context():
+    db = get_db()
+    db.execute('CREATE TABLE IF NOT EXISTS task\
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,title TEXT, description TEXT, start_date )')
+    db.commit()
+
 
 
 
