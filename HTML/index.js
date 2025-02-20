9fc273b62dc536d3ead19252cda8ea0b7d15ea93
MGI <alen.shibu@bscdsh.christuniversity.in>
2025-02-20 09:34:40 +0530

    javascript

--- HTML/index.js
+++ HTML/index.js
@@ -5,3 +5,25 @@ var student = {
     "Class": "BscDS",
     "RollNo": 23112001
 };
+
+console.log(student);
+console.log(student.name);
+
+for(let i=0; i<5;i++){
+    console.log("Joel Is GAY")
+}
+
+let names = ["Alen","Joel","Rosh","max","MGI"];
+console.log(names);
+console.log(typeof(names));
+console.log(names[0]);
+
+
+for(let i=0; i<names.length;i++){
+    console.log(names[i]);
+}
+
+
+names.forEach(name=>{
+    console.log("****",name,"****");
+})
