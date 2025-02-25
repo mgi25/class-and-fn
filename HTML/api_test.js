4460ef23b6c96df09a9852ff1a7f4390fca2ff36
MGI <alen.shibu@bscdsh.christuniversity.in>
2025-02-25 13:54:38 +0530

    populate table

--- HTML/api_test.js
+++ HTML/api_test.js
@@ -5,3 +5,19 @@ const url = "https://api.spaceflightnewsapi.net/v4/articles/"
 fetch(url)
     .then(response => response.json())
     .then(data => console.log(data));
+
+    async function getData() {
+
+        try{
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+            const json = await response.json();
+            console.log(json);
+            return json;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
